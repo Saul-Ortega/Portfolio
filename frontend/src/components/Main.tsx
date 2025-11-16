67fbb1d4672cc69daa68252176c44092033b6ae3
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-11-16 11:29:57 +0000

    Add of mobile responsiveness

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -2,11 +2,11 @@ function Main() {
     
     return(
         <>
-            <main id="inicio" className="scroll-mt-18 sm:scroll-mt-28 bg-[url(home-background.svg)] rounded-3xl xl:w-7xl xl:h-130 mt-10 mx-auto flex flex-col justify-center items-center sm:w-full sm:h-full sm:py-8">
-                <div className="bg-[#fdf6bf] lg:w-4xl lg:h-90 p-5 border-black border-2 grid lg:grid-cols-3 rounded-2xl items-center sm:w-lg sm:h-full sm:grid-cols-1 sm:[grid-template-areas:'a''b'] lg:[grid-template-areas:'a_a_b']">
-                    <div className="flex flex-col justify-center items-center sm:[grid-area:b] lg:[grid-area:a]">
-                        <h3 className="bg-yellow-200 w-fit font-bold text-4xl rounded-2xl lg:p-1 border-black border-2 sm:mt-4 sm:p-2">Hola, soy Saúl</h3>
-                        <div className="bg-white mt-4 border-2 border-black rounded-2xl lg:p-2 mr-4 lg:w-lg sm:w-md sm:p-3">
+            <main id="inicio" className="scroll-mt-18 xs:scroll-mt-20 sm:scroll-mt-28 bg-[url(home-background.svg)] rounded-3xl xl:w-7xl xl:h-130 mt-10 mx-auto flex flex-col justify-center items-center xs:w-full xs:h-full xs:py-8">
+                <div className="bg-[#fdf6bf] lg:w-4xl lg:h-90 p-5 border-black border-2 grid lg:grid-cols-3 rounded-2xl items-center sm:w-lg xs:w-md sm:h-full xs:grid-cols-1 xs:[grid-template-areas:'a''b'] lg:[grid-template-areas:'a_a_b']">
+                    <div className="flex flex-col justify-center items-center xs:[grid-area:b] lg:[grid-area:a]">
+                        <h3 className="bg-yellow-200 w-fit font-bold text-4xl rounded-2xl lg:p-1 border-black border-2 xs:mt-4 xs:p-2">Hola, soy Saúl</h3>
+                        <div className="bg-white mt-4 border-2 border-black rounded-2xl lg:p-2 mr-4 lg:w-lg xs:w-sm xs:p-3 xs:mx-auto">
                             <p>
                                 Disfruto del desarrollo de software porque convierte las <em className="bg-violet-200 p-1 rounded-2xl border border-violet-700">ideas</em> en <em className="bg-violet-200 p-1 rounded-2xl border border-violet-700">soluciones reales</em>.
                             </p>
@@ -20,7 +20,7 @@ function Main() {
                             </a>
                         </div>
                     </div>
-                    <div className="bg-white border-black border-2 p-7 rounded-2xl mx-auto flex flex-col justify-center items-center sm:[grid-area:a] lg:[grid-area:b]">
+                    <div className="bg-white border-black border-2 p-7 rounded-2xl mx-auto flex flex-col justify-center items-center xs:[grid-area:a] lg:[grid-area:b]">
                         <img src="perfil.webp" alt="Foto de perfil" className="w-45 rounded-2xl" />
                     </div>
                 </div>
--- frontend/src/components/Skills.tsx
+++ frontend/src/components/Skills.tsx
@@ -2,44 +2,44 @@ function Skills() {
 
     return (
         <>
-            <section id="habilidades" className="scroll-mt-18 sm:scroll-mt-28 xl:w-7xl mt-15 mx-auto sm:w-full">
-                <h2 className="bg-violet-600/60 border-black border-2 w-fit p-2 text-4xl text-center rounded-2xl sm:mx-auto">Habilidades</h2>
-                <ul className="mt-5 bg-yellow-200/60 grid lg:grid-cols-4 place-items-center gap-y-3 lg:w-3xl p-5 border-black border-2 rounded-2xl mx-auto sm:w-xl sm:grid-cols-3">
+            <section id="habilidades" className="scroll-mt-18 xs:scroll-mt-20 sm:scroll-mt-28 xl:w-7xl mt-15 mx-auto sm:w-full xs:w-md">
+                <h2 className="bg-violet-600/60 border-black border-2 w-fit p-2 text-4xl text-center rounded-2xl xs:mx-auto">Habilidades</h2>
+                <ul className="mt-5 bg-yellow-200/60 grid lg:grid-cols-4 place-items-center gap-y-3 lg:w-3xl p-5 border-black border-2 rounded-2xl mx-auto sm:w-xl sm:grid-cols-3 xs:grid-cols-2 xs:w-md">
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/JavaScript.svg" alt="Logo de JavaScript" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">JavaScript</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">JavaScript</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/React.svg" alt="Logo de React" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">React</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">React</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/TailwindCSS.svg" alt="Logo de Tailwind CSS" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">Tailwind CSS</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Tailwind CSS</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/Java.svg" alt="Logo de Java" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">Java</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Java</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/Kotlin.svg" alt="Logo de Kotlin" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">Kotlin</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Kotlin</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/MySQL.svg" alt="Logo de MySql" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">MySql</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">MySql</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/MongoDB.svg" alt="Logo de MongoDB" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">MongoDB</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">MongoDB</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/Puppeteer.svg" alt="Logo de Puppeteer" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">Puppeteer</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Puppeteer</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/Rxjs.svg" alt="Logo de Rxjs" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold sm:p-2">Rxjs</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Rxjs</h3>
                     </li>
                 </ul>
             </section>
