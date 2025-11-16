3522a343e1655e8cadc5b889b79eb3d7d9e24388
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-11-16 12:29:27 +0000

    Rename skill

--- frontend/src/components/Skills.tsx
+++ frontend/src/components/Skills.tsx
@@ -15,7 +15,7 @@ function Skills() {
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/TailwindCSS.svg" alt="Logo de Tailwind CSS" className="size-28" />
-                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Tailwind CSS</h3>
+                        <h3 className="mt-1 bg-violet-200 w-fit lg:p-1 rounded-2xl border border-violet-700 text-sm font-bold xs:p-2">Tailwind</h3>
                     </li>
                     <li className="flex flex-col items-center p-2 bg-white border-2 border-black rounded-2xl w-10/12 h-fit">
                         <img src="logos/Java.svg" alt="Logo de Java" className="size-28" />
