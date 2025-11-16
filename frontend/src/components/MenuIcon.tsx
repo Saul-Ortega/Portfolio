508015aab016343fd9b80b71d9f5b9cdab8b5394
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-11-16 08:58:30 +0000

    Fix bad written tailwind class

--- frontend/src/components/MenuIcon.tsx
+++ frontend/src/components/MenuIcon.tsx
@@ -8,13 +8,13 @@ function MenuIcon({ open }: Props) {
         <>
             {
                 <div className="relative size-15 flex flex-col justify-center items-center">
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform trainsition-all duration-500 ease-in-out ${
+                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-500 ease-in-out ${
                         open ? "rotate-45" : "top-2"
                     }`}></span>
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform trainsition-all duration-200 ease-in-out ${
+                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-200 ease-in-out ${
                         open ? "opacity-0" : ""
                     }`}></span>
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform trainsition-all duration-500 ease-in-out ${
+                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-500 ease-in-out ${
                         open ? "-rotate-45" : "bottom-2"
                     }`}></span>
                 </div>
