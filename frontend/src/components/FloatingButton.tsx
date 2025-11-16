b37aa492b3da06174006e2776282788801992a1a
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-11-16 13:11:31 +0000

    Add responsiveness to floating button

--- frontend/src/components/FloatingButton.tsx
+++ frontend/src/components/FloatingButton.tsx
@@ -1,8 +1,8 @@
 function FloatingButton() {
     return (
         <>
-            <a href="https://wa.me/34667333742" target="_blank" className="fixed right-5 bottom-5 p-3 bg-[#25d366] rounded-full border-black border-2">
-                <img src="logos/WhatsApp.svg" alt="Icono de WhatsApp" className="w-16 h-16" />
+            <a href="https://wa.me/34667333742" target="_blank" className="fixed right-5 bottom-5 2xs:p-3 xs:p-2.5 bg-[#25d366] rounded-full border-black border-2">
+                <img src="logos/WhatsApp.svg" alt="Icono de WhatsApp" className="2xs:w-16 2xs:h-16 xs:w-12 xs:h-12" />
             </a>
         </>
     )
