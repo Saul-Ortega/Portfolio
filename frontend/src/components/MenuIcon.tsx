c7438fc1478a2781f100be8c0299072f476c4eae
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-11-23 10:23:43 +0000

    Modify animation of Menu Icon

--- frontend/src/components/MenuIcon.tsx
+++ frontend/src/components/MenuIcon.tsx
@@ -7,16 +7,26 @@ function MenuIcon({ open }: Props) {
     return(
         <>
             {
-                <div className="relative size-15 flex flex-col justify-center items-center">
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-500 ease-in-out ${
-                        open ? "rotate-45" : "top-2"
-                    }`}></span>
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-200 ease-in-out ${
-                        open ? "opacity-0" : ""
-                    }`}></span>
-                    <span className={`absolute w-11/12 h-2 bg-black rounded-2xl transform transition-all duration-500 ease-in-out ${
-                        open ? "-rotate-45" : "bottom-2"
-                    }`}></span>
+                <div className="relative size-15 duration-400 ease-cubicbezier">
+                    <div className={`transition-all duration-200 ease-cubic-bezier delay-200 ${
+                        open ? "translate-y-4 delay-150" : "translate-y-0 delay-200"
+                    }`}>
+                        <span className={`absolute w-11/12 h-2 right-0.5 top-2.5 bg-black rounded-2xl duration-200 ease-cubic-bezier ${
+                            open ? "rotate-45 delay-300" : "rotate-0 delay-0"
+                        }`}></span>
+                    </div>
+                    <div className={`transition-all duration-200 ease-cubic-bezier ${
+                        open ? "opacity-0 delay-200" : "opacity-100 delay-200"
+                    }`}>
+                        <span className="absolute w-11/12 h-2 right-0.5 top-6.5 bg-black rounded-2xl"></span>
+                    </div>
+                    <div className={`transition-all duration-200 ease-cubic-bezier delay-200 ${
+                        open ? "-translate-y-4 delay-150" : "translate-y-0 delay-0"
+                    }`}>
+                        <span className={`absolute w-11/12 h-2 right-0.5 top-10.5 bg-black rounded-2xl duration-200 ease-cubic-bezier ${
+                            open ? "-rotate-45 delay-300" : "rotate-0 delay-0"
+                        }`}></span>
+                    </div>
                 </div>
             }
         </>
